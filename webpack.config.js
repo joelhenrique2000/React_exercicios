9c5b4d644943edf50346842d60aa34d504a558c5
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-07-29 21:33:10 -0300

    Primeiro componente

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         port: 5052,
         contentBase: './public',
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
         loaders: [{
             test: /.jsx?$/,

dc3b05eda4ba986934563fd98b3fae77b974f90d
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-07-29 22:02:50 -0300

    Primeiro componente com Arrow function

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-export default function() {
-    return <h1>Primeiro Componente!</h1>
-}
+export default () => (
+    <h1>Primeiro Componente!</h1>
+)

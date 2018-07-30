9fc6295f72c831f96b4b42598a695ed82e00d722
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-07-29 22:17:21 -0300

    exportando mais de um componente

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Component from './component'
+import {Primeiro, Segundo} from './component'
 
 ReactDOM.render(
-    <Component value='show!'/>,
+   <div>
+       <Primeiro />
+       <Segundo />
+   </div>,
     document.getElementById('app')
 )
